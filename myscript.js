07d55659553ada3c629425d48b82a11a675fe5c4
bishalpal <bishaldg47@gmail.com>
2023-09-28 20:39:59 +0530

    add details for fetching data from api.

--- myscript.js
+++ myscript.js
@@ -1,18 +1,32 @@
 console.log("Script running okay!");
 
-const url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_sGXjfx1tGzZRGVtZ8bZM3c4rdeelCGgZGYjWYIY1";
+const populate = async(base, value) =>{
+    const url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_sGXjfx1tGzZRGVtZ8bZM3c4rdeelCGgZGYjWYIY1&base_currency=" + base;
+    
+    let response = await fetch(url);
+    let rjson = await response.json();
+    console.log(rjson);
+
+    for(let key in Object.keys(rjson["data"])) {
+        
+    }
+    
+    let tablebody1 = document.getElementById("table1");
+    tablebody1.innerHTML = `
+    <tr>
+    <td>data 1</td>
+    <td>data 2</td>
+    </tr>
+    `;
+    
+};
 
 let mybtn = document.querySelector("button");
 mybtn.addEventListener("click", (e) =>{
     e.preventDefault();
     console.log("button is clicked !");
+    const value = parseInt(document.querySelector("input[name='amount']").value);
+    const base_currency = document.querySelector("select[name='currency_base']").value;
+
+    populate(base_currency, value);
 });
-
-
-let tablebody1 = document.getElementById("table1");
-tablebody1.innerHTML = `
-<tr>
-<td>data 1</td>
-<td>data 2</td>
-</tr>
-`;
