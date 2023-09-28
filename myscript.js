94bfb21452a24a62f4559a9877e0ab032ac068f4
bishalpal <bishaldg47@gmail.com>
2023-09-28 19:45:20 +0530

    add more details

--- myscript.js
+++ myscript.js
@@ -1,3 +1,18 @@
 console.log("Script running okay!");
 
 const url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_sGXjfx1tGzZRGVtZ8bZM3c4rdeelCGgZGYjWYIY1";
+
+let mybtn = document.querySelector("button");
+mybtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+    console.log("button is clicked !");
+});
+
+
+let tablebody1 = document.getElementById("table1");
+tablebody1.innerHTML = `
+<tr>
+<td>data 1</td>
+<td>data 2</td>
+</tr>
+`;
